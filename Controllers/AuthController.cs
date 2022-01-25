cf28ce1967a871d216204fa8b780218d3402fee7
Maxi Jabase <maxijabase@gmail.com>
2022-01-25 12:10:21 -0300

    initial commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using LucesCasa.Backend;
+using LucesCasa.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LucesCasa.Api.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        private readonly BEAuth _authHelper;
+
+        public AuthController(BEAuth authHelper)
+        {
+            _authHelper = authHelper;
+        }
+
+        [HttpPost, Route("login")]
+        public IActionResult Login(LoginDTO model)
+        {
+            if (model == null)
+            {
+                return BadRequest("Invalid login request");
+            }
+            var token = _authHelper.Authorize(model);
+            if (token == null)
+            {
+                return Unauthorized();
+            }
+            else
+            {
+                return Ok(token);
+            }
+        }
+    }
+}
---
+++ Controllers/LucesController.cs
@@ -0,0 +1,24 @@
+﻿using LucesCasa.Backend;
+using LucesCasa.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LucesCasa.Api.Controllers
+{
+    public class LucesController : Controller
+    {
+        private readonly BELuces _lucesHelper;
+        public LucesController(BELuces lucesHelper)
+        {
+            _lucesHelper = lucesHelper;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> EnviarInstruccion(InstruccionDTO ins)
+        {
+            _lucesHelper.ToString();
+        }
+    }
+}
